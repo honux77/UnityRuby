57f8708e4508c13989e72608eaeccfe07a17de4d
Hoyoung Jung <honux77@gmail.com>
2020-12-12 23:46:31 +0900

    Apply FixUpdate() for Ruby

--- Assets/Scritps/RubyController.cs
+++ Assets/Scritps/RubyController.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+
+    public float speed = 7.0f;
+    float horizontal;
+    float vertical;
+
+    Rigidbody2D rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
         // QualitySettings.vSyncCount = 0;
-        // Application.targetFrameRate = 10;        
+        // Application.targetFrameRate = 10;   
+        rigidbody2d = GetComponent<Rigidbody2D>();  
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
         //Debug.Log(horizontal);
+
+    }
+
+    void FixedUpdate() 
+    {
         Vector2 position = transform.position;
-        position.x += 7.0f * horizontal * Time.deltaTime;
-        position.y += 7.0f * vertical * Time.deltaTime;
-        transform.position = position; 
+        position.x += speed * horizontal * Time.deltaTime;
+        position.y += speed * vertical * Time.deltaTime;
+        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
 
     }
 }
