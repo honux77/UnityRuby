73aae612e68f5d1026b403c76ad222d093972a0d
Hoyoung Jung <honux77@gmail.com>
2020-12-15 23:15:51 +0900

    Add Ruby animation

--- Assets/Scritps/RubyController.cs
+++ Assets/Scritps/RubyController.cs
@@ -15,13 +15,16 @@ public class RubyController : MonoBehaviour
     float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
 
-    // Start is called before the first frame update
-    void Start()
+        // Start is called before the first frame update
+        void Start()
     {
         // QualitySettings.vSyncCount = 0;
         // Application.targetFrameRate = 10;   
-        rigidbody2d = GetComponent<Rigidbody2D>();        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,6 +32,18 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
