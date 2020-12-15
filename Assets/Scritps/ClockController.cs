5f51537ed10236643d48351ef5e559ece6180ef8
Hoyoung Jung <honux77@gmail.com>
2020-12-15 22:49:31 +0900

    Add enemy animation

--- Assets/Scritps/ClockController.cs
+++ Assets/Scritps/ClockController.cs
@@ -13,16 +13,18 @@ public class ClockController : MonoBehaviour
     float timer;
 
     Rigidbody2D rbody;
+    Animator animator;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
         timer = changeTime;
-
+        animator = GetComponent<Animator>();
     }
-
-    void OnCollisionEnter2D(Collision2D other)
+    
+    void OnCollisionEnter2D(Collision2D other) 
     {
         RubyController player = other.gameObject.GetComponent<RubyController>();
 
@@ -50,10 +52,14 @@ public class ClockController : MonoBehaviour
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
             position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
 
         rbody.MovePosition(position);
